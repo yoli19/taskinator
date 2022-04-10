58eaefb839c9bf867996302d56bfa670c8818165
Yolanda Jacobs <y.jacobs@hotmail.com>
2022-04-09 20:58:15 -0400

    submit function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
     taskItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(taskItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
